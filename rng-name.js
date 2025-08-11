ef99c0baac680d0a0badc3ac9c81cb01cbb1123e
Lilly <108250586+Teloky@users.noreply.github.com>
2025-08-11 15:47:05 -0500

    Update rng-name.js

--- rng-name.js
+++ rng-name.js
@@ -1,7 +1,8 @@
-const names = [ "carlos", "marianne", "Nhuamai", "erika", "Casandra", "Blair", "ekho", "naomi", "jesus", "gael", "kazi", "rosie", "fany", "emilio", "nicko"]
+const names = [ "carlos", "marianne", "Nhuamai", "erika", "Casandra", "Blair", "ekho", "naomi", "jesus", "gael", "kazi", "rosie", "fany", "emilio"]
 const number=Math.floor(Math.random() * names.length);
 document.getElementById("Name").textContent = names[number] = "You sadly going call " +  names[number] + ".";
 document.getElementById("generate").addEventListener("click", function() {
     const number = Math.floor(Math.random() * names.length);
     document.getElementById("Name").textContent = "You sadly going call " + names[number] + ".";
+
 })
